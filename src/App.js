80bc08e9eda3f34102796491e8aeb459935aceb5
josebetancourtzam18 <josebetancourtzam18@gmail.com>
2022-09-05 17:10:41 -0400

    Creando componentes de rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Box} from '@mui/material'
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from 'components/index';
 
-function App() {
-  return (
-    <div>
-      <h1>Prueba</h1>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+  <Box sx={{backgroundColor: '#000'}}>
+    <Navbar/>
+    <Routes>
+      <Route path='/' exact element={<Feed/>}/>
+      <Route path='/video/:id' element={<VideoDetail/>}/>
+      <Route path='/channel/:id' element={<ChannelDetail/>}/>
+      <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+    </Routes>
+  </Box>
+  </BrowserRouter>
+)
 
 export default App;
